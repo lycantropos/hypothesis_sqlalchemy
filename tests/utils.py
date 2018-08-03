792a8aa436b513f9d97c50b77365b4fca660666e
Ibrakov Azat <azatibrakov@gmail.com>
2018-08-03 16:48:15 +0300

    Fix formatting

--- tests/utils.py
+++ tests/utils.py
@@ -22,8 +22,9 @@ def example(strategy: SearchStrategy) -> Any:
 def table_record_is_valid(table_record: RecordType,
                           *,
                           table: Table) -> bool:
-    return all(coordinate is None and column.nullable or
-               isinstance(column.type, UUID) and
-               isinstance(coordinate, uuid.UUID) or
-               isinstance(coordinate, column.type.python_type)
+    return all(coordinate is None
+               and column.nullable
+               or isinstance(column.type, UUID)
+               and isinstance(coordinate, uuid.UUID)
+               or isinstance(coordinate, column.type.python_type)
                for coordinate, column in zip(table_record, table.columns))
