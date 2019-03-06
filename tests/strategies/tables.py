54ae5bd20f0da6f0c138aea8374845de43d2ab5d
Josh Rosen <rosenville@gmail.com>
2019-03-06 00:08:58 -0800

    Update tests.strategies.tables to generate tables without unique columns

--- tests/strategies/tables.py
+++ tests/strategies/tables.py
@@ -1,7 +1,18 @@
+from operator import attrgetter
+
 from hypothesis import strategies
 from sqlalchemy import MetaData
 
-from hypothesis_sqlalchemy import tables
+from hypothesis_sqlalchemy import (columns,
+                                   tables)
 
 metadata = MetaData()
-tables = tables.factory(metadatas=strategies.just(metadata))
+non_unique_columns = columns.non_primary_keys_factory(
+        are_unique=strategies.just(False))
+tables_without_unique_columns = tables.factory(
+        metadatas=strategies.just(metadata),
+        columns_lists=strategies.lists(non_unique_columns,
+                                       unique_by=attrgetter('name')))
+tables_with_unique_columns = tables.factory(
+        metadatas=strategies.just(metadata))
+tables = tables_without_unique_columns | tables_with_unique_columns
