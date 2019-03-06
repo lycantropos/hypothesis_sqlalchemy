a2bd0cba84632378900b38737530071402435dae
Josh Rosen <rosenville@gmail.com>
2019-03-05 19:09:06 -0800

    Add failing regression test.

--- tests/test_tables/test_records.py
+++ tests/test_tables/test_records.py
@@ -1,7 +1,10 @@
 from functools import partial
 
 from hypothesis.searchstrategy import SearchStrategy
-from sqlalchemy.schema import Table
+from sqlalchemy.schema import (Column,
+                               MetaData,
+                               Table)
+from sqlalchemy.sql.sqltypes import Integer
 
 from hypothesis_sqlalchemy.tables import records
 from tests.utils import (example,
@@ -20,6 +23,19 @@ def test_table_records_factory(table: Table) -> None:
 
 def test_table_records_lists_factory(table: Table) -> None:
     table_records_lists = records.lists_factory(table)
+    _do_test_table_records_lists_factory(table, table_records_lists)
+
+
+def test_table_records_lists_factory_for_table_without_primary_key() -> None:
+    metadata = MetaData()
+    table = Table('numbers', metadata, Column('number', Integer))
+    # min_size = 2 is added as a regression test for a corner-case where the
+    # absence of a primary key prevented multiple records from being generated:
+    table_records_lists = records.lists_factory(table, min_size=2)
+    _do_test_table_records_lists_factory(table, table_records_lists)
+
+
+def _do_test_table_records_lists_factory(table: Table, table_records_lists: SearchStrategy) -> None:
     table_records = example(table_records_lists)
 
     assert isinstance(table_records_lists, SearchStrategy)
