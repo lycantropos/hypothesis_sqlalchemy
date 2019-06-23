deb35d694937fe428e1ef49b8e785d5898346406
Ibrakov Azat <azatibrakov@gmail.com>
2019-06-23 11:01:54 +0300

    Complete 'Column'-related strategies factories

--- hypothesis_sqlalchemy/columns/base.py
+++ hypothesis_sqlalchemy/columns/base.py
@@ -1,9 +1,11 @@
 from typing import (Any,
                     Callable,
-                    List)
+                    List,
+                    Optional)
 
 from hypothesis import strategies
 from sqlalchemy.schema import Column
+from sqlalchemy.sql.type_api import TypeEngine
 
 from hypothesis_sqlalchemy.hints import Strategy
 from hypothesis_sqlalchemy.utils import (identifiers,
@@ -11,13 +13,14 @@ from hypothesis_sqlalchemy.utils import (identifiers,
 from . import types
 
 
-def non_primary_keys_factory(*,
-                             names: Strategy = identifiers,
-                             types: Strategy = types.factory(),
-                             are_unique: Strategy = strategies.booleans(),
-                             are_nullable: Strategy = strategies.booleans(),
-                             are_indexed: Strategy = strategies.booleans()
-                             ) -> Strategy:
+def non_primary_keys_factory(
+        *,
+        names: Strategy[str] = identifiers,
+        types: Strategy[TypeEngine] = types.factory(),
+        are_unique: Strategy[bool] = strategies.booleans(),
+        are_nullable: Strategy[bool] = strategies.booleans(),
+        are_indexed: Strategy[bool] = strategies.booleans()
+) -> Strategy[Column]:
     return strategies.builds(Column,
                              name=names,
                              type_=types,
@@ -27,11 +30,12 @@ def non_primary_keys_factory(*,
                              index=are_indexed)
 
 
-def primary_keys_factory(*,
-                         names: Strategy = identifiers,
-                         types: Strategy = types.primary_keys_factory(),
-                         are_auto_incremented: Strategy = strategies.just(True)
-                         ) -> Strategy:
+def primary_keys_factory(
+        *,
+        names: Strategy[str] = identifiers,
+        types: Strategy[TypeEngine] = types.primary_keys_factory(),
+        are_auto_incremented: Strategy[bool] = strategies.just(True)
+) -> Strategy:
     return strategies.builds(Column,
                              name=names,
                              type_=types,
@@ -39,34 +43,43 @@ def primary_keys_factory(*,
                              primary_key=strategies.just(True))
 
 
-def lists_factory(*,
-                  primary_keys: Strategy = primary_keys_factory(),
-                  non_primary_keys: Strategy = non_primary_keys_factory(),
-                  max_size: int = None) -> Strategy:
-    max_size = max_size - 1 if max_size is not None else max_size
-    non_primary_keys_lists = strategies.lists(non_primary_keys,
-                                              min_size=1,
-                                              max_size=max_size)
+def lists_factory(
+        *,
+        primary_keys: Strategy[str] = primary_keys_factory(),
+        non_primary_keys: Strategy[TypeEngine] = non_primary_keys_factory(),
+        min_size: int = 0,
+        max_size: Optional[int] = None) -> Strategy[List[Column]]:
+    if min_size > 0:
+        min_size -= 1
+    if max_size is not None:
+        max_size -= 1
+    rest_columns_lists = strategies.lists(non_primary_keys,
+                                          min_size=min_size,
+                                          max_size=max_size)
 
-    def list_factory(draw: Callable[[Strategy], Any]) -> List[Column]:
+    def to_columns_lists(draw: Callable[[Strategy], Any]) -> List[Column]:
         primary_key = draw(primary_keys)
 
-        def names_are_unique(columns: List[Column]) -> int:
+        def names_are_unique(columns: List[Column]) -> bool:
             names = [primary_key.name]
             names += [column.name for column in columns]
             return len(names) == len(set(names))
 
-        non_primary_keys_list = draw(non_primary_keys_lists
-                                     .filter(names_are_unique))
-        return [primary_key] + non_primary_keys_list
+        rest_columns_list = draw(rest_columns_lists
+                                 .filter(names_are_unique))
+        return [primary_key] + rest_columns_list
 
-    return strategies.composite(list_factory)()
+    return strategies.composite(to_columns_lists)()
 
 
-def non_all_unique_lists_factory(lists: Strategy = lists_factory()
-                                 ) -> Strategy:
+def non_all_unique_lists_factory(
+        min_size: int = 0,
+        max_size: Optional[int] = None
+) -> Strategy:
     def has_non_unique_column(columns: List[Column]) -> bool:
         return any(not is_column_unique(column)
                    for column in columns)
 
-    return lists.filter(has_non_unique_column)
+    return (lists_factory(min_size=min_size,
+                          max_size=max_size)
+            .filter(has_non_unique_column))
