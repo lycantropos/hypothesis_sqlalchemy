ea251478754ba6e5dcde885e00e04f442d4d4748
Azat Ibrakov <azatibrakov@gmail.com>
2019-06-27 13:10:29 +0300

    Complete tests utils

--- tests/utils.py
+++ tests/utils.py
@@ -1,7 +1,4 @@
-import uuid
-
 from sqlalchemy import Table
-from sqlalchemy.dialects.postgresql import UUID
 
 from hypothesis_sqlalchemy.hints import RecordType
 
@@ -18,7 +15,5 @@ def table_record_is_valid(table_record: RecordType,
                           table: Table) -> bool:
     return all(coordinate is None
                and column.nullable
-               or isinstance(column.type, UUID)
-               and isinstance(coordinate, uuid.UUID)
                or isinstance(coordinate, column.type.python_type)
                for coordinate, column in zip(table_record, table.columns))
