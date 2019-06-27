491a019afed754682c62108ed74d0b0010ffff25
Azat Ibrakov <azatibrakov@gmail.com>
2019-06-27 14:47:47 +0300

    Complete test for 'tabular.factory' strategy factory

--- tests/tabular_tests/base_tests/test_factory.py
+++ tests/tabular_tests/base_tests/test_factory.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from hypothesis import given
 from sqlalchemy.schema import (MetaData,
                                Table)
@@ -9,18 +11,31 @@ from tests.utils import DataObject
 from . import strategies
 
 
-@given(strategies.metadatas_strategies)
-def test_basic(metadatas: Strategy[MetaData]) -> None:
-    result = factory(metadatas=metadatas)
+@given(strategies.metadatas_strategies,
+       strategies.min_sizes, strategies.max_sizes)
+def test_basic(metadatas: Strategy[MetaData],
+               min_size: int,
+               max_size: Optional[int]) -> None:
+    result = factory(metadatas=metadatas,
+                     min_size=min_size,
+                     max_size=max_size)
 
     assert isinstance(result, Strategy)
 
 
-@given(strategies.data, strategies.metadatas_strategies)
-def test_examples(data: DataObject, metadatas: Strategy[MetaData]) -> None:
-    strategy = factory(metadatas=metadatas)
+@given(strategies.data, strategies.metadatas_strategies,
+       strategies.min_sizes, strategies.max_sizes)
+def test_examples(data: DataObject,
+                  metadatas: Strategy[MetaData],
+                  min_size: int,
+                  max_size: Optional[int]) -> None:
+    strategy = factory(metadatas=metadatas,
+                       min_size=min_size,
+                       max_size=max_size)
 
     result = data.draw(strategy)
 
     assert isinstance(result, Table)
+    assert min_size <= len(result.columns)
+    assert max_size is None or len(result.columns) <= max_size
     assert not all(map(is_column_unique, result.columns))
