0c5f2ead13ce76b7389bd3dcb52ec557e5e9673c
Azat Ibrakov <azatibrakov@gmail.com>
2019-06-27 14:47:47 +0300

    Complete test for 'tabular.factory' strategy factory

--- tests/tabular_tests/base_tests/strategies.py
+++ tests/tabular_tests/base_tests/strategies.py
@@ -1,7 +1,11 @@
 from hypothesis import strategies
 
 from tests.strategies import (data,
-                              metadatas)
+                              max_sizes,
+                              metadatas,
+                              min_sizes)
 
 data = data
+min_sizes = min_sizes
+max_sizes = max_sizes
 metadatas_strategies = strategies.just(metadatas)
