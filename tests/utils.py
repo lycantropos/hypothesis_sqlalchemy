e64eca2666b176ae8bc018fef00c83802d0668a9
Ruben Helsloot <rht670@student.vu.nl>
2019-07-12 14:25:31 +0200

    Move assert uniqueness to utils file

--- tests/utils.py
+++ tests/utils.py
@@ -1,6 +1,9 @@
+from typing import List
+
 from sqlalchemy import Table
 
 from hypothesis_sqlalchemy.hints import RecordType
+from hypothesis_sqlalchemy.utils import is_column_unique
 
 try:
     from hypothesis.strategies import DataObject
@@ -17,3 +20,13 @@ def table_record_is_valid(table_record: RecordType,
                and column.nullable
                or isinstance(coordinate, column.type.python_type)
                for coordinate, column in zip(table_record, table.columns))
+
+def records_satisfy_table_constraints(records: List[RecordType],
+                                      *,
+                                      table: Table) -> bool:
+    unique_indices = [index
+                      for index, column in enumerate(table.columns)
+                      if is_column_unique(column)]
+    # All returned values are unique for the columns marked as unique
+    return all(len({record[index] for record in records}) == len(records)
+               for index in unique_indices)
