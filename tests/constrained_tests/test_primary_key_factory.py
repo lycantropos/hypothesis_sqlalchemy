78c03be3d6bc884151fc77044edd32ddf7dc60de
Azat Ibrakov <azatibrakov@gmail.com>
2019-09-26 19:17:57 +0300

    Complete test for 'constrained.primary_keys_factory' strategy factory

--- tests/constrained_tests/test_primary_key_factory.py
+++ tests/constrained_tests/test_primary_key_factory.py
@@ -1,4 +1,5 @@
-from typing import List
+from typing import (List,
+                    Tuple)
 
 from hypothesis import given
 from sqlalchemy.schema import (Column,
@@ -6,24 +7,42 @@ from sqlalchemy.schema import (Column,
 
 from hypothesis_sqlalchemy.constrained import primary_keys_factory
 from hypothesis_sqlalchemy.hints import Strategy
-from tests.utils import DataObject
+from tests.utils import (Bounds,
+                         DataObject)
 from . import strategies
 
 
-@given(strategies.columns_lists)
-def test_basic(columns: List[Column]) -> None:
-    result = primary_keys_factory(columns)
+@given(strategies.columns_lists_with_bounds)
+def test_basic(columns_with_bounds: Tuple[List[Column], Bounds]) -> None:
+    columns, (min_size, max_size) = columns_with_bounds
+
+    result = primary_keys_factory(columns,
+                                  min_size=min_size,
+                                  max_size=max_size)
 
     assert isinstance(result, Strategy)
 
 
-@given(strategies.data, strategies.columns_lists)
+@given(strategies.data, strategies.columns_lists_with_bounds)
 def test_examples(data: DataObject,
-                  columns: List[Column]) -> None:
-    strategy = primary_keys_factory(columns)
+                  columns_with_bounds: Tuple[List[Column], Bounds]) -> None:
+    columns, (min_size, max_size) = columns_with_bounds
+
+    strategy = primary_keys_factory(columns,
+                                    min_size=min_size,
+                                    max_size=max_size)
 
     result = data.draw(strategy)
 
+    existing_primary_key_columns = [column
+                                    for column in columns
+                                    if column.primary_key]
+
     assert isinstance(result, PrimaryKeyConstraint)
+    assert len(result.columns) >= min(len(existing_primary_key_columns),
+                                      min_size)
+    assert (max_size is None
+            or len(result.columns) <= max(len(existing_primary_key_columns),
+                                          max_size))
     assert all(column in columns
                for column in result.columns)
