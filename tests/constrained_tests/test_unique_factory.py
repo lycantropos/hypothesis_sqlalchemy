7e082296533667b5aa6fc34887c988b84c6a20a6
Azat Ibrakov <azatibrakov@gmail.com>
2019-09-26 19:16:37 +0300

    Complete test for 'constrained.unique_factory' strategy factory

--- tests/constrained_tests/test_unique_factory.py
+++ tests/constrained_tests/test_unique_factory.py
@@ -1,4 +1,5 @@
-from typing import List
+from typing import (List,
+                    Tuple)
 
 from hypothesis import given
 from sqlalchemy.schema import (Column,
@@ -6,23 +7,35 @@ from sqlalchemy.schema import (Column,
 
 from hypothesis_sqlalchemy.constrained import unique_factory
 from hypothesis_sqlalchemy.hints import Strategy
-from tests.utils import DataObject
+from tests.utils import (Bounds,
+                         DataObject)
 from . import strategies
 
 
-@given(strategies.columns_lists)
-def test_basic(columns: List[Column]) -> None:
-    result = unique_factory(columns)
+@given(strategies.columns_lists_with_bounds)
+def test_basic(columns_with_bounds: Tuple[List[Column], Bounds]) -> None:
+    columns, (min_size, max_size) = columns_with_bounds
+
+    result = unique_factory(columns,
+                            min_size=min_size,
+                            max_size=max_size)
 
     assert isinstance(result, Strategy)
 
 
-@given(strategies.data, strategies.columns_lists)
-def test_examples(data: DataObject, columns: List[Column]) -> None:
-    strategy = unique_factory(columns)
+@given(strategies.data, strategies.columns_lists_with_bounds)
+def test_examples(data: DataObject,
+                  columns_with_bounds: Tuple[List[Column], Bounds]) -> None:
+    columns, (min_size, max_size) = columns_with_bounds
+
+    strategy = unique_factory(columns,
+                              min_size=min_size,
+                              max_size=max_size)
 
     result = data.draw(strategy)
 
     assert isinstance(result, UniqueConstraint)
+    assert len(result.columns) >= min_size
+    assert max_size is None or len(result.columns) <= max_size
     assert all(column in columns
                for column in result.columns)
