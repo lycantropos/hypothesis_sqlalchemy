8124cc5d110a2b11fa6477da4b7a0b351d00e8cc
Azat Ibrakov <azatibrakov@gmail.com>
2019-09-26 18:18:14 +0300

    Complete 'constrained.primary_key_factory' strategy factory

--- hypothesis_sqlalchemy/constrained/base.py
+++ hypothesis_sqlalchemy/constrained/base.py
@@ -38,18 +38,13 @@ def unique_factory(columns: List[Column]) -> Strategy[UniqueConstraint]:
                  UniqueConstraint(*constraint_columns)))
 
 
-def primary_keys_factory(columns: List[Column],
-                         *,
-                         min_size: int = 1) -> Strategy[PrimaryKeyConstraint]:
-    if not columns:
-        raise ValueError('`columns` should not be empty')
-    if min_size < 1:
-        raise ValueError('`min_size` should be at least 1')
+def primary_keys_factory(columns: List[Column]
+                         ) -> Strategy[PrimaryKeyConstraint]:
     existing_columns = [column for column in columns if column.primary_key]
     if existing_columns:
         return strategies.just(PrimaryKeyConstraint(*existing_columns))
-    return (strategies.sets(strategies.sampled_from(columns),
-                            min_size=min_size,
+    return (strategies.sets(strategies.sampled_from(columns) if columns
+                            else strategies.nothing(),
                             max_size=len(columns))
             .map(lambda constraint_columns:
                  PrimaryKeyConstraint(*constraint_columns)))
