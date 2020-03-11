f7c0fc4dba2b8736e3d07a3202695fcc4614d554
Pau Tallada Crespí <tallada@pic.es>
2020-03-11 13:49:55 +0100

    Updated to work with latest hypothesis >= 4.53.2.

--- hypothesis_sqlalchemy/tabular/records.py
+++ hypothesis_sqlalchemy/tabular/records.py
@@ -1,6 +1,5 @@
 from typing import Optional
 
-from hypothesis.searchstrategy.collections import TupleStrategy
 from sqlalchemy import Table
 
 from hypothesis_sqlalchemy import columnar
@@ -8,7 +7,7 @@ from hypothesis_sqlalchemy.hints import Strategy
 
 
 def factory(table: Table,
-            **fixed_columns_values: Strategy) -> TupleStrategy:
+            **fixed_columns_values: Strategy) -> Strategy:
     return columnar.records.factory(table.columns,
                                     **fixed_columns_values)
 
@@ -17,7 +16,7 @@ def lists_factory(table: Table,
                   *,
                   min_size: int = 0,
                   max_size: Optional[int] = None,
-                  **fixed_columns_values: Strategy) -> TupleStrategy:
+                  **fixed_columns_values: Strategy) -> Strategy:
     return columnar.records.lists_factory(table.columns,
                                           table.constraints,
                                           min_size=min_size,
