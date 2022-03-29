76283e4060b0759ff0aaa035733b0ad277dab25e
Azat Ibrakov <azatibrakov@gmail.com>
2022-03-29 22:22:46 +0700

    Complete docs

--- hypothesis_sqlalchemy/scheme.py
+++ hypothesis_sqlalchemy/scheme.py
@@ -1,3 +1,5 @@
+"""Strategies for generating ``SQLAlchemy`` objects."""
+
 from .core import (column as _column,
                    column_type as _column_type,
                    table as _table)
