3c6fc2e9d07d4e28c3ae879289bf08dd41c1dea8
Azat Ibrakov <azatibrakov@gmail.com>
2022-03-29 11:29:19 +0700

    Complete tests configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,23 @@
+import os
+from datetime import timedelta
+
+import pytest
 from hypothesis import (HealthCheck,
                         settings)
 
+on_ci = bool(os.getenv('CI', False))
+max_examples = settings.default.max_examples
 settings.register_profile('default',
-                          deadline=None,
+                          deadline=(timedelta(hours=1) / max_examples
+                                    if on_ci
+                                    else None),
+                          max_examples=max_examples,
                           suppress_health_check=[HealthCheck.too_slow,
                                                  HealthCheck.filter_too_much])
+
+
+@pytest.hookimpl(trylast=True)
+def pytest_sessionfinish(session: pytest.Session,
+                         exitstatus: pytest.ExitCode) -> None:
+    if exitstatus == pytest.ExitCode.NO_TESTS_COLLECTED:
+        session.exitstatus = pytest.ExitCode.OK
