b55309b74d0bf1c196df6253f7b69788976ea8f7
Azat Ibrakov <azatibrakov@gmail.com>
2022-03-29 16:24:24 +0700

    Complete tests configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,21 @@
 import os
+import platform
 from datetime import timedelta
 
 import pytest
 from hypothesis import (HealthCheck,
                         settings)
 
+is_pypy = platform.python_implementation() == 'PyPy'
 on_ci = bool(os.getenv('CI', False))
 max_examples = settings.default.max_examples
 settings.register_profile('default',
                           deadline=(timedelta(hours=1) / max_examples
                                     if on_ci
                                     else None),
-                          max_examples=max_examples,
+                          max_examples=(10
+                                        if is_pypy and on_ci
+                                        else max_examples),
                           suppress_health_check=[HealthCheck.too_slow,
                                                  HealthCheck.filter_too_much])
 
